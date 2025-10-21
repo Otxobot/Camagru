5256c4c51e92f6628eaf2590cf57c59753dd7ced
Aiert Basante <otxoboy64@gmail.com>
2025-10-21 19:23:33 +0200

    i think i connected to the db

--- app/config/config.php
+++ app/config/config.php
@@ -1,8 +1,18 @@
 <?php
 require_once __DIR__ . '/../../vendor/autoload.php';
 
+if (!file_exists(__DIR__ . '/../../.env')) {
+    die('.env file not found');
+}
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
-$dotenv->load();
+
+try {
+    $dotenv->load();
+    $dotenv->required(['DB_HOST', 'DB_PORT', 'DB_NAME', 'DB_USER', 'DB_PASS'])->notEmpty();
+} catch (Exception $e) {
+    die('Environment variables not properly set: ' . $e->getMessage());
+}
 
 define('DB_HOST', $_ENV['DB_HOST']);
 define('DB_PORT', $_ENV['DB_PORT']);
